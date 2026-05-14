12a66e72e072991a282cc89ff09bceb35e9e8f85
Jyotiranjan Mohanty <jyotimohanty289@gmail.com>
2026-05-14 15:18:44 +0530

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js
+const CACHE_NAME = "oak-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/custom.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
